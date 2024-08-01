3c85c8dcc067ba7d205195b2d38aa3e412007258
ogeail <paul0411@naver.com>
2024-08-01 14:45:54 +0900

    gpt model first request

---
+++ 0gptapi_text.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#os.environ.get 부분을 변수에 넣지 않고 gpt_api_key 부분에 그냥 넣어도 됨.
+OPEN_API_KEY = os.environ.get("OPEN_API_KEY")
+
+gpt_api_key = OPEN_API_KEY
+
+client = OpenAI(
+    # api_key="gpt api key 넣기"
+    api_key=gpt_api_key
+)
+#print("인증성공")
+
+#Send question to GPT Model
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system","content": "You are an IT expert."},
+        {"role":"user","content":"What is top 5 popular Computer Language in the world?"}
+    ]
+)
+
+print(response.choices[0].message.content)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gptapi_ex
+
+# 설치 라이브러리
+'''
+
+pip install --upgrade openai
+
+'''
